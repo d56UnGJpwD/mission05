e4c4deccf00afeb5e6e11b2b96ea6eb2d2ae7dab
Kirill Brainard <braikiri@isu.edu>
2019-02-23 21:55:20 -0700

    RecursiveBinarySearch Complete

--- src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveBinarySearch.java
+++ src/main/java/edu/isu/cs/cs3308/algorithms/impl/RecursiveBinarySearch.java
@@ -7,6 +7,32 @@ public class RecursiveBinarySearch implements ArraySearch
     @Override
     public <E extends Comparable> int search(E[] array, E item)
     {
-        return 0;
+        if(array == null || array.length == 0 || item == null)
+        {
+            return -1;
+        }
+        return recBinarySearch(array, item, 0, array.length - 1);
+    }
+
+    public <E extends Comparable> int recBinarySearch(E[] array, E item, int floor, int ceiling)
+    {
+        int index = (floor + ceiling)/2;
+
+        if(floor >= ceiling)
+        {
+            return -1;
+        }
+        if(array[index] == item)
+        {
+            return index;
+        }
+        if((Integer)item < (Integer)array[index])
+        {
+            return recBinarySearch(array, item, floor, index -1);
+        }
+        else
+        {
+            return recBinarySearch(array, item, index+1, ceiling);
+        }
     }
 }
