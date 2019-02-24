c6f9abbe12d33accfec13067823a2d1fb41ecf86
Kirill Brainard <braikiri@isu.edu>
2019-02-23 21:43:04 -0700

    Binary Search Complete

--- src/main/java/edu/isu/cs/cs3308/algorithms/impl/BinarySearch.java
+++ src/main/java/edu/isu/cs/cs3308/algorithms/impl/BinarySearch.java
@@ -7,6 +7,30 @@ public class BinarySearch implements ArraySearch
     @Override
     public <E extends Comparable> int search(E[] array, E item)
     {
-        return 0;
+        if(array == null || array.length == 0 || item == null)
+        {
+            return -1;
+        }
+
+        int floor = 0;
+        int ceiling = array.length - 1;
+
+        while(floor <= ceiling)
+        {
+            int index = (floor + ceiling)/2;
+            if(array[index] == item)
+            {
+                return index;
+            }
+            else if((Integer)item < (Integer)array[index])
+            {
+                ceiling = index - 1;
+            }
+            else
+            {
+                floor = index + 1;
+            }
+        }
+        return -1;
     }
 }
